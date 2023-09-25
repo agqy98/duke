b80caa60d15d34b9c1d0395fff007f192289c995
agqy98 <anxelxoh@gmail.com>
2023-09-25 21:15:35 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,102 +1,167 @@
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Duke {
-    private static int count = 0;
+    private static int list_count = 0;
     private static Task[] list = new Task[100];
+    private static final Map<TaskType, String> TASK_KEYWORDS = new HashMap<>();
+
+    static {
+        TASK_KEYWORDS.put(TaskType.TODO, "todo");
+        TASK_KEYWORDS.put(TaskType.DEADLINE, "deadline");
+        TASK_KEYWORDS.put(TaskType.EVENT, "event");
+    }
 
     public static void main(String[] args) {
-        printHelloWorld();
+        printWelcomeMessage();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("");
-        String input = scanner.nextLine();
-        while (!input.equalsIgnoreCase("bye")) {
-            if (input.equalsIgnoreCase("list")) {
-                printList();
-            } else {
-                handleInput(input);
-            }
-            input = scanner.nextLine();
-        }
-        printByeWorld();
+        String input;
+
+        do {
+            input = scanner.nextLine().trim();
+            processInput(input);
+        } while (!input.equalsIgnoreCase("bye"));
+
+        printGoodbyeMessage();
         scanner.close();
     }
 
-    private static void formatter() {
-        System.out.print("    ");
+    private static void processInput(String input) {
+        if (input.equalsIgnoreCase("list")) {
+            printTaskList();
+        } else {
+            TaskType taskType = getTaskType(input);
+            if (taskType == null) {
+                printErrorMessage(ErrorType.ERR_SYSTEM_READ_FAIL, taskType);
+                return;
+            } else {
+                ErrorType err = hasError(input, taskType);
+                if (err != null) {
+                    printErrorMessage(err, taskType);
+                    return;
+                } else {
+                    try {
+                        System.out.println(input);
+                        System.out.println(TASK_KEYWORDS.get(taskType).length());
+                        String taskDescription = input.substring(TASK_KEYWORDS.get(taskType).length() + 1);
+                        if (taskDescription.isEmpty()) {
+                            printErrorMessage(ErrorType.ERR_EMPTY_DESCRIPTION, taskType);
+                            return;
+                        } else {
+                            taskDescription = taskDescription.substring(1);
+                            switch (taskType) {
+                                case EVENT:
+                                    addTask(new Events(taskDescription));
+                                    break;
+                                case DEADLINE:
+                                    addTask(new Deadlines(taskDescription));
+                                    break;
+                                default:
+                                    addTask(new ToDos(taskDescription));
+                            }
+                        }
+                    } catch (IllegalArgumentException e) {
+                        // Handle invalid format error
+                        printErrorMessage(ErrorType.ERR_INVALID_FORMAT, taskType);
+                        return;
+                    }
+                }
+            }
+        }
     }
 
-    private static void printList() {
+    private static ErrorType hasError(String input, TaskType t) {
+        // Input begins with "[command]"
+        String command = TASK_KEYWORDS.get(t);
+        if (input.trim().equalsIgnoreCase(command)) {
+            return ErrorType.ERR_EMPTY_DESCRIPTION;
+        } else if (input.trim().length() > command.length() &&
+                !input.substring(command.length(), command.length() + 1).equals(" ")) {
+            return ErrorType.ERR_POSSIBLE_TYPO;
+        } else {
+            return null;
+        }
+
+    }
+
+    private static TaskType getTaskType(String input) {
+        for (Map.Entry<TaskType, String> entry : TASK_KEYWORDS.entrySet()) {
+            if (input.toLowerCase().startsWith(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    private static void addTask(Task task) {
+        list[list_count] = task;
+        list_count++;
         printSeparator();
-        for (int i = 0; i < count; i++) {
-            formatter();
-            System.out.print(i + 1);
-            System.out.print(". ");
+        System.out.println("Got it. I've added this task:");
+        task.print();
+        System.out.println("Now you have " + list_count + " task(s) in the list");
+        printSeparator();
+    }
+
+    private static void printTaskList() {
+        printSeparator();
+        for (int i = 0; i < list_count; i++) {
+            System.out.print(i + 1 + ". ");
             list[i].print();
         }
         printSeparator();
     }
 
-    private static boolean validateInput(String input, String command) {
-        int size = command.length();
-        return input.length() > size && input.substring(0, size).equalsIgnoreCase(command);
-    }
-
-    private static void handleInput(String input) {
-        input = input.trim();
-        Boolean isT = validateInput(input, "todo");
-        Boolean isD = validateInput(input, "deadline");
-        Boolean isE = validateInput(input, "event");
-
-        if (isT || isD || isE) {
-            Task item;
-            if (isT) {
-                item = new ToDos(input.substring(5));
-            } else if (isD) {
-                item = new Deadlines(input.substring(9));
-            } else {
-                item = new Events(input.substring(6));
-            }
-            list[count] = item;
-            count++;
-
-            printSeparator();
-            formatter();
-            System.out.println("Got it. I've added this task:");
-            formatter();
-            formatter();
-            item.print();
-            formatter();
-            System.out.println("Now you have " + count + " task(s) in the list");
-            printSeparator();
-
-        } else {
-            printSeparator();
-            formatter();
-            System.out.println("Invalid command");
-            printSeparator();
-        }
-    }
-
-    private static void printSeparator() {
-        formatter();
-        System.out.println("____________________________________________________________");
-    }
-
-    private static void printHelloWorld() {
+    private static void printWelcomeMessage() {
         printSeparator();
-        formatter();
         System.out.println("Hello! I'm AngelBot!");
-        formatter();
         System.out.println("What can I do for you?");
         printSeparator();
     }
 
-    private static void printByeWorld() {
+    private static void printGoodbyeMessage() {
         printSeparator();
-        formatter();
         System.out.println("Bye. Hope to see you again soon!");
         printSeparator();
+    }
 
+    private static void printErrorMessage(ErrorType e, TaskType t) {
+        printSeparator();
+        switch (e) {
+            case ERR_EMPTY_DESCRIPTION:
+                System.out.println("OOPS!!! The description of a " + TASK_KEYWORDS.get(t) + " cannot be empty.");
+                break;
+            case ERR_INVALID_FORMAT:
+                System.out.printf(
+                        "OOPS! It seems you didn't provide a valid %s format. To create a %s task, use the following format:",
+                        TASK_KEYWORDS.get(t), TASK_KEYWORDS.get(t));
+                System.out.println("");
+                switch (t) {
+                    case DEADLINE:
+                        System.out.println("deadline [description] /by [date]");
+                        break;
+                    case EVENT:
+                        System.out.println("event [description] /from [start time] /to [end time]");
+                        break;
+                    default:
+                        System.out.println("todo [description]"); // Not expecting any formatting error here
+                        break;
+                }
+                break;
+            case ERR_POSSIBLE_TYPO:
+                System.out.printf("OOPS! It appears there might be a typo. Did you mean to write '%s'?",
+                        TASK_KEYWORDS.get(t));
+                System.out.println("");
+                break;
+            default:
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+        printSeparator();
+    }
+
+    private static void printSeparator() {
+        System.out.println("____________________________________________________________");
     }
 }
---
+++ src/main/java/ErrorType.java
@@ -0,0 +1,6 @@
+public enum ErrorType {
+    ERR_EMPTY_DESCRIPTION, 
+    ERR_POSSIBLE_TYPO,
+    ERR_INVALID_FORMAT,
+    ERR_SYSTEM_READ_FAIL, 
+};
